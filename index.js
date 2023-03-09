708046d62cdde100fb037ca6f7700cede57545cf
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-03-09 18:30:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const names=["Ada", "Brendan", "Ali"];
+const x="birthday";
+
+function writeCards(names, x){
+     
+    let result =[ ];
+       
+    for (let i = 0; i < names.length; i++){
+        
+        result.push(`Thank you, ${names[i]}, for the wonderful ${x} gift!`);
+        // debugger;
+    }
+    return result;
+}
+    //et returnedValue=writeCards(names, x);
+    console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+
+
+    
+    function countDown(num){
+        while (num >= 0) {
+        console.log(num--);
+        }
+
+    }
+
+     countDown()
